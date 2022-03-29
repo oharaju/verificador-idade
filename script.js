e2dd4ecdb550c236fd00aa8475dfac1f0a410448
Júlia Ohara <juliaohara.br@gmail.com>
2022-03-29 16:32:11 -0300

    insert new script to file

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ function verificar() {
   const data = new Date()
   const ano = data.getFullYear() //Retorna um número c quatro digitos
   const form = document.querySelector('input.anoNascimento')
-  const resul = document.querySelector('p.resultado')
+  const resul = document.querySelector('div.resultado')
 
   if (form.value.length == 0 || form.value > ano) { //verifica se o input está vazio e/ou se o ano digitado é maior que o ano atual
     window.alert('Preencha seus dados')
   } else {
-    const sexo = document.querySelector('input.sex')
-    const idade = ano - Number(form.value)
-    resul.innerHTML = (`Sua idade é de ${idade}anos`)
+    const sexo = document.getElementsByName('sex')
+    const idade = ano - Number(form.value) //ano atual menos ano digitado pelo usuario
+
+    if (sexo[0].checked) {
+      genero = 'Homem'
+    } else {
+      genero = 'Mulher'
+    }
+    resul.innerHTML = `Detectamos ${genero} com ${idade} anos`
   }
 }
