5d8ba5376c323cbfd0a2cafc45f618f38649fab0
Júlia Ohara <juliaohara.br@gmail.com>
2022-03-30 00:01:01 -0300

    refactor code structure and insert images

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function verificar() {
   const data = new Date()
   const ano = data.getFullYear() //Retorna um número c quatro digitos
   const form = document.querySelector('input.anoNascimento')
-  const resul = document.querySelector('div.resultado')
+  const resul = document.querySelector('p.resultado')
 
   if (form.value.length == 0 || form.value > ano) { //verifica se o input está vazio e/ou se o ano digitado é maior que o ano atual
     window.alert('Preencha seus dados')
   } else {
     const sexo = document.getElementsByName('sex')
     const idade = ano - Number(form.value) //ano atual menos ano digitado pelo usuario
-    const img = document.createElement('img')
-    img.setAttribute('id', 'img')
+    const img = document.createElement('img') // cria img dinamicamente
+    img.setAttribute('id', 'img') //Atribui o elemento img ao id
 
     if (sexo[0].checked) {
       genero = 'Homem'
@@ -27,18 +27,18 @@ function verificar() {
 
     } else {
       genero = 'Mulher'
-      if (idade <=0 && idade <10) {
-        // CRIANCA
-      } else if (idade <21) {
-        // JOVEM
-      } else if (idade < 50) {
-        // ADULTO
-      } else {
-        // IDOSO
+      if (idade >=0 && idade < 10) { // CRIANCA
+        img.setAttribute('src', 'img/foto-bebe-f.png')
+      } else if (idade < 21) { // JOVEM
+        img.setAttribute('src', 'img/foto-jovem-f.png')
+      } else if (idade < 50) { // ADULTO
+        img.setAttribute('src', 'img/foto-adulto-f.png')
+      } else { // IDOSO
+        img.setAttribute('src', 'img/foto-idoso-f.png')
       }
     }
     resul.style.textAlign = 'center'
     resul.innerHTML = `<p>Detectamos ${genero} com ${idade} anos</p>`
-    resul.appendChild(img)
+    resul.appendChild(img) //inseri o elemento img na estrutura do dom
   }
 }
